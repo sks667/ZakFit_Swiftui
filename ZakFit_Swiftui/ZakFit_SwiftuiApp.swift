588004028faf3abfaa144f6779465aff9fec0e41
sks667 <lahlouh.sophian@gmail.com>
2024-12-08 03:27:54 +0100

    Initial Commit

---
+++ ZakFit_Swiftui/ZakFit_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFit_SwiftuiApp.swift
+//  ZakFit_Swiftui
+//
+//  Created by Apprenant 178 on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZakFit_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZakFit_SwiftuiTests/ZakFit_SwiftuiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFit_SwiftuiTests.swift
+//  ZakFit_SwiftuiTests
+//
+//  Created by Apprenant 178 on 08/12/2024.
+//
+
+import Testing
+@testable import ZakFit_Swiftui
+
+struct ZakFit_SwiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
